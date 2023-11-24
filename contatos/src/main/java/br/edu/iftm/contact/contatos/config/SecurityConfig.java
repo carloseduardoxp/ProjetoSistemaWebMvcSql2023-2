6944956f496967b2066b113f711ae5756966efab
Your Name <you@example.com>
2023-11-24 20:23:23 -0300

    Implementando spring security para controlar acesso

---
+++ contatos/src/main/java/br/edu/iftm/contact/contatos/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package br.edu.iftm.contact.contatos.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                auth -> auth.requestMatchers("/", "/login/**").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin((form) -> form.loginPage("/").permitAll() 
+             ) 
+
+             .csrf(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+}
--- contatos/src/main/java/br/edu/iftm/contact/contatos/controller/LoginController.java
+++ contatos/src/main/java/br/edu/iftm/contact/contatos/controller/LoginController.java
@@ -1,6 +1,15 @@
 package br.edu.iftm.contact.contatos.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,6 +17,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.edu.iftm.contact.contatos.domain.Login;
 import br.edu.iftm.contact.contatos.service.LoginService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class LoginController {
@@ -24,20 +36,39 @@ public class LoginController {
         return "login";
     }
 
-    @PostMapping("newUser")
+    @PostMapping("/login/novoUsuario")
     public String salvarLogin(Login login, Model model) {
         loginService.salvar(login);
         model.addAttribute("mensagem", "Usuario salvo com sucesso");
-        return "login";
+        return home(model);
     }
 
-    @PostMapping("login")
-    public String login(Login login, Model model) {
+    @PostMapping("/login/entrar")
+    public String login(HttpSession session, Login login, Model model) {
         if (loginService.verificaSenha(login)) {
+            List<GrantedAuthority> authorities = AuthorityUtils.createAuthorityList("ADMIN");
+            Authentication authentication = new UsernamePasswordAuthenticationToken(
+                    login.getUsuario(),
+                    login.getSenha(),
+                    authorities);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            session.setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY,
+                    SecurityContextHolder.getContext());
+
             return contatoController.getContatos(model);
         } else {
             model.addAttribute("mensagem", "Usuario ou senha inválidos");
             return "login";
         }
     }
+
+    @RequestMapping("/login/sair")
+    public String customLogoutMethod(HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "login";
+    }
+
 }
